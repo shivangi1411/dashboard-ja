15b53ab1063617e20829052f31fb4e0348a8f45e
Shivangi Goel <shivangi.goel@accenture.com>
2020-02-19 15:14:34 +0530

    Updates

---
+++ precache-manifest.0683b2f0a02600a50d644a330a5b3fe8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e16bcb6ad3e60a9d9dcb24ce23149df",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "46322dd66b47775f6b7b",
+    "url": "/myapp/static/css/2.5597ae33.chunk.css"
+  },
+  {
+    "revision": "ee8716513137746aa781",
+    "url": "/myapp/static/css/main.79a03582.chunk.css"
+  },
+  {
+    "revision": "46322dd66b47775f6b7b",
+    "url": "/myapp/static/js/2.66f67826.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/myapp/static/js/2.66f67826.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee8716513137746aa781",
+    "url": "/myapp/static/js/main.1abc5613.chunk.js"
+  },
+  {
+    "revision": "95173a08ff60e6cbed51",
+    "url": "/myapp/static/js/runtime-main.ae6f2750.js"
+  },
+  {
+    "revision": "b1c1d5a22f650f7f2b26a539f2a0a50d",
+    "url": "/myapp/static/media/decrease.b1c1d5a2.png"
+  },
+  {
+    "revision": "c1c8e6359b39bb49d81f8d3dd3c76471",
+    "url": "/myapp/static/media/downloads.c1c8e635.png"
+  },
+  {
+    "revision": "b2d411330cffb0744d1b2325b9de6978",
+    "url": "/myapp/static/media/growth.b2d41133.png"
+  },
+  {
+    "revision": "dbdc7134afc2f4ea027d1aaae8d504d7",
+    "url": "/myapp/static/media/neext.dbdc7134.PNG"
+  },
+  {
+    "revision": "70025f4d1b767aa8e0b8a1aa425f91f9",
+    "url": "/myapp/static/media/smartphone.70025f4d.png"
+  },
+  {
+    "revision": "2d5c5bd067fc0cf916b26f781b9dbb25",
+    "url": "/myapp/static/media/upload.2d5c5bd0.png"
+  }
+]);
